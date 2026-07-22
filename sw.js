777dda1443d4f5c0d13fcfa49786215a6065ac6f
020404clc <2547156003@qq.com>
2026-07-22 16:06:39 +0800

    v120: add fresh reset

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - 离线缓存 v111 (同步保护+防反馈循环)
-const CACHE_NAME='jb-salary-v119';
+const CACHE_NAME='jb-salary-v120';
 const urlsToCache = ['./', './index.html'];
 
 self.addEventListener('install', event => {
