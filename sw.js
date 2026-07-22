08d2597dd352b81371f3a7c8532f2402de262910
020404clc <2547156003@qq.com>
2026-07-22 18:18:55 +0800

    v129: fix sync merge strategy & data persistence

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - 离线缓存 v126
-const CACHE_NAME='jb-salary-v128';
+const CACHE_NAME='jb-salary-v129';
 const urlsToCache = ['./', './index.html', './app.html'];
 
 self.addEventListener('install', event => {
