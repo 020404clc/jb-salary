31f9ff7c34f256ad4e9eca6602cafbc7800b09dc
020404clc <2547156003@qq.com>
2026-07-22 14:25:11 +0800

    v111 SW

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Service Worker - 离线缓存 v109 (实时同步修复版)
-const CACHE_NAME = 'jb-salary-v110';
+// Service Worker - 离线缓存 v111 (同步保护+防反馈循环)
+const CACHE_NAME = 'jb-salary-v111';
 const urlsToCache = ['./', './index.html'];
 
 self.addEventListener('install', event => {
