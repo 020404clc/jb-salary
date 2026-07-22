1044abf0e56cc9d876ca776b0f5fd92407c6f7a1
020404clc <2547156003@qq.com>
2026-07-22 16:03:41 +0800

    chore: 升级缓存版本到 v119 - 修复乱码和登录问题

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - 离线缓存 v111 (同步保护+防反馈循环)
-const CACHE_NAME='jb-salary-v118';
+const CACHE_NAME='jb-salary-v119';
 const urlsToCache = ['./', './index.html'];
 
 self.addEventListener('install', event => {
