0991e8a7059e3bfe69f987d963c5d05392fd0f03
020404clc <2547156003@qq.com>
2026-07-23 17:27:45 +0800

    v141: room autocomplete datalist - sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - 离线缓存 v133
-const CACHE_NAME='jb-salary-v140';
+const CACHE_NAME='jb-salary-v141';
 const urlsToCache = ['./', './index.html', './app.html', './z-new.html', './fresh.html'];
 
 self.addEventListener('install', event => {
