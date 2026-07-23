f85b6de471cb010b608b66e8f3ba8e2fdc766bdc
020404clc <2547156003@qq.com>
2026-07-23 22:00:57 +0800

    v152: fix admin login - pre-init Supabase client before login

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - 离线缓存 v146
-const CACHE_NAME='jb-salary-v151';
+const CACHE_NAME='jb-salary-v152';
 const urlsToCache = ['./', './index.html', './app.html', './z-new.html', './fresh.html'];
 
 self.addEventListener('install', event => {
