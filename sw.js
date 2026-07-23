82d657841d45c1a08861c9d726e3f3210d2d919f
020404clc <2547156003@qq.com>
2026-07-24 05:58:24 +0800

    v171: fix sync - field mapping + dirty table clear + sync timeout

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Service Worker - 离线缓存 v166
-const CACHE_NAME='jb-salary-v170';
+// Service Worker - 离线缓存 v171
+const CACHE_NAME='jb-salary-v171';
 const urlsToCache = ['./', './index.html'];
 
 self.addEventListener('install', event => {
