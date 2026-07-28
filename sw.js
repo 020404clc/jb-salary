35dbac366430bd3130616afe5be45841ece76a5c
020404clc <2547156003@qq.com>
2026-07-28 16:44:28 +0800

    v183: SW cache bump

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - 离线缓存 v171
-const CACHE_NAME='jb-salary-v182';
+const CACHE_NAME='jb-salary-v183';
 const urlsToCache = ['./', './index.html'];
 
 self.addEventListener('install', event => {
