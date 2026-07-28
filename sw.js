b7b7ea8b2d124f9753fa53fedaac2f5a431cdba1
020404clc <2547156003@qq.com>
2026-07-28 15:37:10 +0800

    v180: SW cache upgrade

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - 离线缓存 v171
-const CACHE_NAME='jb-salary-v179';
+const CACHE_NAME='jb-salary-v180';
 const urlsToCache = ['./', './index.html'];
 
 self.addEventListener('install', event => {
