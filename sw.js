cddf1d775bf24554245aa3d1ea4d546be2823ee9
020404clc <2547156003@qq.com>
2026-07-28 16:52:49 +0800

    v185: 自动同步增强 - 去掉手动按钮, 添加脏表重试, 可见同步状态

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - 离线缓存 v171
-const CACHE_NAME='jb-salary-v184';
+const CACHE_NAME='jb-salary-v185';
 const urlsToCache = ['./', './index.html'];
 
 self.addEventListener('install', event => {
