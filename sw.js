a1fc79457d7195022c3ee2dd6649f28a9c2fc70b
020404clc <2547156003@qq.com>
2026-07-28 17:55:10 +0800

    v186: 自动同步增强 - 添加上传锁+下载保护窗口，防止并发冲突和数据覆盖

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - 离线缓存 v171
-const CACHE_NAME='jb-salary-v185';
+const CACHE_NAME='jb-salary-v186';
 const urlsToCache = ['./', './index.html'];
 
 self.addEventListener('install', event => {
