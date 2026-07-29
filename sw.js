e6cde229a9452e5d7dd37d5630991920a80c93a1
020404clc <2547156003@qq.com>
2026-07-29 19:24:14 +0800

    v191: 修复通用表同步删除顺序（upsert成功后再删除，确保数据一致性）

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - 离线缓存 v171
-const CACHE_NAME='jb-salary-v190';
+const CACHE_NAME='jb-salary-v191';
 const urlsToCache = ['./', './index.html'];
 
 self.addEventListener('install', event => {
